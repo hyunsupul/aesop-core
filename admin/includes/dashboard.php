f3ed79a8e267bdc3e532e85c34bb48694d134272
Bearded Avenger <email@nickhaskins.com>
2013-12-10 13:29:30 -0600

    wording

--- admin/includes/dashboard.php
+++ admin/includes/dashboard.php
@@ -6,11 +6,6 @@ class aiCoreDash {
 
 		add_action('wp_dashboard_setup', array($this,'remove_widgets' ));
 		add_action('wp_dashboard_setup', array($this,'custom_widgets'));
-
-		add_action('wp_logout', array($this,'whoisonline_logged_out'));
-		add_action('wp_login', array($this,'whoisonline_logged_in'), 10, 2);
-		add_filter( 'heartbeat_received',  array($this,'whoisonline_check_who_is_online'), 10, 3 );
-		add_action( 'admin_enqueue_scripts', array($this,'whoisonline_load_scripts') );
 	}
 
 	/**
@@ -92,7 +87,7 @@ class aiCoreDash {
 		    	endwhile;
 
 		   else:
-		   		echo 'no stories1';
+		   		_e('You havent created any stories yet. <a href="/wp-admin/post-new.php">Create one</a> now.', 'aesop-core');
 		   	endif;
 
 		  	wp_reset_query();
@@ -147,7 +142,7 @@ class aiCoreDash {
 		    	endwhile;
 
 		   else:
-		   		echo 'no stories1';
+		   		_e('You havent been invited to collaborate on any stories yet.', 'aesop-core');
 		   	endif;
 
 		  	wp_reset_query();
@@ -184,73 +179,6 @@ class aiCoreDash {
 		?></ul><?php
 	}
 
-	function whoisonline_logged_out(){
-	    $user_id = get_current_user_id();
-	    update_user_meta( $user_id, 'whoisonline_is_online', false );
-	}
-
-
-	function whoisonline_logged_in( $username, $user ){
-	   	update_user_meta( $user->ID, 'whoisonline_is_online', true );
-	    update_user_meta( $user->ID, 'whoisonline_last_active', time() );
-	}
-	/**
-	 * Returns an array of usernames indexed by user IDs of the users who are currently online
-	 * @param array $args Optional array to adapt query
-	 * @return array
-	*/
-	function who_is_online( $args = array() ) {
-
-          //Get users active in last 24 hours
-        $args = wp_parse_args( $args, array(
-            'meta_key' => 'whoisonline_last_active',
-            'meta_value' => time() - 24*60*60,
-            'meta_compare' => '>',
-            'count_total' => false,
-        ));
-        $users = get_users( $args );
-
-        //Initiate array
-        $online_users = array();
-
-        foreach( $users as $user ) {
-            if( ! get_user_meta( $user->ID, 'whoisonline_is_online', true ) )
-                 continue;
-
-            $online_users[$user->ID] = $user->user_login;
-        }
-
-        return $online_users;
-	}
-
-	/**
-	 * Hearbeat server-side handling. Hooked onto 'heartbeat_received'
-	 * Checks first that the data from the browser is requesting "who's online"
-	 * If so, returns an array of user names (indexed by ID)
-	*/
-	function whoisonline_check_who_is_online( $response, $data, $screen_id) {
-
-        //Update user's activity
-        $user_id = get_current_user_id();
-        update_user_meta( $user_id, 'whoisonline_last_active', time() );
-        update_user_meta( $user_id, 'whoisonline_is_online', true );
-
-        if( !empty( $data['who-is-online'] ) ){
-
-                //Attach data to be sent
-                $response['whoisonline'] = $this->who_is_online();
-
-        }
-        return $response;
-	}
-	function whoisonline_load_scripts() {
-
-	        /* Ony load scripts when you need to - in this case, everywhere if the user is logged-in */
-	        if( is_user_logged_in() ){
-	                wp_enqueue_script( 'whoisonline-jquery-notice', AI_CORE_URL. '/admin/assets/js/jquery.notice.js', array( 'jquery' ) );        
-	                wp_enqueue_script( 'whoisonline',  AI_CORE_URL. '/admin/assets/js/who-is-online.js', array( 'heartbeat', 'whoisonline-jquery-notice' ) );        
-	        }
-	}
 
 }
 new aiCoreDash;
