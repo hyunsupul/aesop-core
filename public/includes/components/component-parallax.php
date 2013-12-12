81cc8a5e3d8e93942dbd50fbe0f5b5475f92c2ba
Bearded Avenger <email@nickhaskins.com>
2013-12-11 19:47:47 -0600

    do floater, reorganize, do filters

--- public/includes/components/component-parallax.php
+++ public/includes/components/component-parallax.php
@@ -5,37 +5,51 @@ if (!function_exists('aesop_parallax_shortcode')){
 	function aesop_parallax_shortcode($atts, $content = null) {
 
 		$defaults = array(
-			'img' => 'http://placekitten.com/1200/700',
-			'height' => 500,
-			'speed'	=> 0.3,
-			'floater' => false,
-			'floatermedia' => '',
-			'floaterposition' => 'right',
-			'cappos' => 'bottom-left',
-			'lightbox' => false
+			'img' 				=> 'http://placekitten.com/1200/700',
+			'height' 			=> 500,
+			'parallaxbg' 		=> 'on',
+			'parallaxspeed'		=> 0.3,
+			'floater' 			=> false,
+			'floatermedia' 		=> '',
+			'floaterposition' 	=> 'right',
+			'captionposition' 	=> 'bottom-left',
+			'lightbox' 			=> false
 		);
-		$atts = shortcode_atts($defaults, $atts);
 
+		$atts = apply_filters('aesop_parallax_defaults',shortcode_atts($defaults, $atts));
+
+		$out = '';
 		$hash = rand();
 
-		$style = sprintf('style="background-image:url(\'%s\');background-size:cover;"',$atts['img'],$atts['height']);
-		$lblink = 'on' == $atts['lightbox'] ? sprintf('<a class="aesop-lb-link swipebox" rel="lightbox" title="%s" href="%s"><i class="sorencon sorencon-search-plus">+</i></a>',do_shortcode($content),$atts['img']) : false;
+		// final destination of the floater is half the height of the viewport that's set
+		$floaterdestination = $atts['height'] / 2;
 
-		$floater = 'on' == $atts['floater'] ? sprintf('<div class="aesop-parallax-sc-floater floater-%s">%s</div>', $atts['floaterposition'], $atts['floatermedia']) : false;
+		$laxclass 	= 'on' == $atts['parallaxbg'] ? 'is-parallax' : false;
+		$style 		= sprintf('style="background-image:url(\'%s\');background-size:cover;"',$atts['img'],$atts['height']);
+		$lblink 	= 'on' == $atts['lightbox'] ? sprintf('<a class="aesop-lb-link swipebox" rel="lightbox" title="%s" href="%s"><i class="sorencon sorencon-search-plus">+</i></a>',do_shortcode($content),$atts['img']) : false;
+		$floater 	= 'on' == $atts['floater'] ? sprintf('<div class="aesop-parallax-sc-floater floater-%s" data-top-bottom="margin-top:20px;" data-bottom-top="margin-top:%spx;">%s</div>', $atts['floaterposition'], $floaterdestination, $atts['floatermedia']) : false;
 
-		$out = sprintf('<script>
-		jQuery(document).ready(function(){
+		// Call Parallax Method if Set
+		if ('on' == $atts['parallaxbg']) {
+			$out .= sprintf('<script>
+			jQuery(document).ready(function(){
 
 		   		jQuery(\'.aesop-parallax-sc.aesop-parallax-sc-%s .aesop-parallax-sc-img\').parallax({
 		    		speed: %s
 		    	});
-		});
-		</script>',$hash,$atts['speed']);
-		$out .= sprintf('<section class="aesop-parallax-sc aesop-parallax-sc-%s" style="height:%s;">', $hash, $atts['height']);
+			});
+			</script>',$hash,$atts['parallaxspeed']);
+		}
 
+		$out 	.= sprintf('<section class="aesop-parallax-sc aesop-parallax-sc-%s" style="height:%s;">
+								%s
+								<div class="aesop-parallax-sc-caption-wrap %s">
+									<div class="aesop-parallax-sc-caption">%s</div>
+								</div>
+								%s
+								<div class="aesop-parallax-sc-img %s" %s></div>
+							</section>',$hash, $atts['height'], $floater, $atts['captionposition'], do_shortcode($content), $lblink, $laxclass, $style);
 
-		$out .= sprintf('%s<div class="aesop-parallax-sc-caption-wrap %s"><div class="aesop-parallax-sc-caption">%s</div></div>%s<div class="aesop-parallax-sc-img" %s></div></section>', $floater, $atts['cappos'], do_shortcode($content), $lblink, $style);
-
-		return $out;
+		return apply_filters('aesop_parallax_output',$out);
 	}
 }
