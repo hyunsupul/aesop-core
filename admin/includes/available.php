db06b199e5febc72c158a183a645e3621a4c6320
Bearded Avenger <email@nickhaskins.com>
2013-12-11 20:01:43 -0600

    organize and i81n

--- admin/includes/available.php
+++ admin/includes/available.php
@@ -6,153 +6,182 @@
 if(!function_exists('aesop_shortcodes')){
 	function aesop_shortcodes( $shortcode = null ) {
 		$shortcodes = array(
-			'parallax' => array(
-				'name' => 'Aesop Parallax',
-				'type' => 'wrap',
-				'atts' => array(
-					'img' => array(
-						'values' => array( ),
-						'default' => '#',
-						'desc' => __( 'Image', 'aesop-shortcode-generator' ),
+			'parallax' 				=> array(
+				'name' 				=> __('Aesop Parallax', 'aesop-core'),
+				'type' 				=> 'wrap',
+				'atts' 				=> array(
+					'img' 			=> array(
+						'values' 	=> array( ),
+						'default' 	=> '#',
+						'desc' 		=> __( 'Image', 'aesop-core' ),
 					),
-					'height' => array(
-						'values' => array(),
-						'default' => '500',
-						'desc' => __('Height of Image Area', 'aesop-shortcode-generator' )
+					'height' 		=> array(
+						'values' 	=> array(),
+						'default' 	=> '500',
+						'desc' 		=> __('Height of Image Area', 'aesop-core' )
 					),
-					'cappos' => array(
-						'values' => array(
-							'bottom-left',
-							'bottom-right',
-							'top-left',
-							'top-right',
+					'parallaxbg' 	=> array(
+						'values' 	=> array(
+							__('on', 'aesop-core'),
+							__('off', 'aesop-core')
 						),
-						'default' => 'bottom-left',
-						'desc' => __('Caption Position', 'aesop-shortcode-generator' )
+						'default' 	=> 'on',
+						'desc' 		=> __('Parallax Background Image', 'aesop-core' )
 					),
-					'speed' => array(
-						'values' => array(),
-						'default' => '0.15',
-						'desc' => __( 'Speed of Parallax Effect', 'aesop-shortcode-generator' )
+					'parallaxspeed' => array(
+						'values' 	=> array(),
+						'default' 	=> '0.3',
+						'desc' 		=> __('Parallax Background Speed', 'aesop-core' )
 					),
-					'lightbox' => array(
-						'values' => array(
-							'on',
-							'off'
+					'floater' 		=> array(
+						'values'	=> array(
+							__('true', 'aesop-core'),
+							__('false', 'aesop-core')
 						),
-						'default' => 'off',
-						'desc' => __('Image Lightbox', 'aesop-shortcode-generator' )
-					)
-				),
-				'content' => __( 'Caption', 'ba-shortcodes' ),
-				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Parallax styled image component with caption and optional lightbox.','aesop-shortcode-generator' )
-			),
-			'quote' => array(
-				'name' => 'Aesop Quote Section',
-				'type' => 'wrap',
-				'atts' => array(
-					'background' => array(
-						'values' => array( ),
-						'type'	=> 'color',
-						'default' => '#282828',
-						'desc' => __( 'Hex Color of Background', 'aesop-shortcode-generator' )
+						'default' 	=> 'false',
+						'desc' 		=> __('Enable Floating Element', 'aesop-core' )
 					),
-					'text' => array(
-						'values' => array(),
-						'type'	=> 'color',
-						'default' => '#FFFFFF',
-						'desc' => __('Hex Color of Text', 'aesop-shortcode-generator' )
+					'floatermedia' 	=> array(
+						'values' 	=> array(),
+						'default' 	=> 'false',
+						'desc' 		=> __('Floater Media', 'aesop-core' )
 					),
-					'height' => array(
-						'values' => array(),
-						'default' => '',
-						'desc' => __('Height of Image Area', 'aesop-shortcode-generator' )
-					),
-				),
-				'content' => __( 'Quote Here', 'ba-shortcodes' ),
-				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Section quote area with background and color controls.','aesop-shortcode-generator' )
-			),
-			'image_back' => array(
-				'name' => 'Image Back',
-				'type' => 'wrap',
-				'atts' => array(
-					'img' => array(
-						'values' => array( ),
-						'default' => '#',
-						'desc' => __( 'Image URL', 'aesop-shortcode-generator' )
-					),
-					'color' => array(
-						'values' => array(),
-						'type'	=> 'color',
-						'default' => '#FFFFFF',
-						'desc' => __('Color of Text', 'aesop-shortcode-generator' )
-					)
-				),
-				'content' => __( 'Your normal content goes here.', 'ba-shortcodes' ),
-				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Creates a background image section, with text in the middle.','aesop-shortcode-generator' )
-			),
-			'columns' => array(
-				'name' => 'Magazine Columns',
-				'type' => 'wrap',
-				'atts' => array(
-					'num' => array(
-						'values' => array( ),
-						'default' => 2,
-						'desc' => __( 'Number of Columns', 'aesop-shortcode-generator' )
-					)
-				),
-				'content' => __( 'All your normal text goes here.', 'ba-shortcodes' ),
-				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Wraps your text in magazine style columns.','aesop-shortcode-generator' )
-			),
-			'chapter_head' => array(
-				'name' => 'Chapter Heading',
-				'type' => 'single',
-				'atts' => array(
-					'num' => array(
-						'values' => array( ),
-						'default' => 1,
-						'desc' => __( 'What Chapter is This?', 'aesop-shortcode-generator' )
-					)
-				),
-				'content' => __( 'Null here', 'ba-shortcodes' ),
-				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Creates the scroll to point, as a chapter heading.','aesop-shortcode-generator' )
-			),
-			'video' => array(
-				'name' => 'Video Section',
-				'type' => 'single',
-				'atts' => array(
-					'src' => array(
-						'values' => array(
-							'vimeo',
-							'youtube',
-							'kickstarter',
-							'viddler',
-							'dailymotion'
+					'floaterposition' => array(
+						'values' 	=> array(
+							__('right', 'aesop-core'),
+							__('left', 'aesop-core')
 						),
-						'default' => 'vimeo',
-						'desc' => __('Video Source', 'aesop-shortcode-generator' )
+						'default' 	=> 'right',
+						'desc' 		=> __('Position of Floater', 'aesop-core' )
 					),
-					'id' => array(
-						'values' => array( ),
-						'default' => '',
-						'desc' => __( 'Video ID', 'aesop-shortcode-generator' )
+					'captionposition' => array(
+						'values' 	=> array(
+							__('bottom-left', 'aesop-core'),
+							__('bottom-right', 'aesop-core'),
+							__('top-left', 'aesop-core'),
+							__('top-right', 'aesop-core')
+						),
+						'default' 	=> 'bottom-left',
+						'desc' 		=> __('Caption Position', 'aesop-core' )
+					),
+					'lightbox' 		=> array(
+						'values' 	=> array(
+							__('on', 'aesop-core'),
+							__('off', 'aesop-core')
+						),
+						'default' 	=> 'off',
+						'desc' 		=> __('Image Lightbox', 'aesop-core' )
 					)
 				),
-				'content' => __( 'Null here', 'ba-shortcodes' ),
-				'usage' => '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
-				'desc' => __( 'Creates a video section.','aesop-shortcode-generator' )
+				'content' 			=> __( 'Caption', 'ba-shortcodes' ),
+				'usage' 			=> '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' 				=> __( 'Parallax styled image component with caption and optional lightbox.','aesop-core' )
+			),
+			'quote' 				=> array(
+				'name' 				=> __('Aesop Quote Section', 'aesop-core'),
+				'type' 				=> 'wrap',
+				'atts' 				=> array(
+					'background' 	=> array(
+						'values' 	=> array( ),
+						'type'		=> 'color',
+						'default' 	=> '#282828',
+						'desc' 		=> __( 'Hex Color of Background', 'aesop-core' )
+					),
+					'text'			 => array(
+						'values' 	=> array(),
+						'type'		=> 'color',
+						'default' 	=> '#FFFFFF',
+						'desc' 		=> __('Hex Color of Text', 'aesop-core' )
+					),
+					'height' 		=> array(
+						'values'	=> array(),
+						'default' 	=> '',
+						'desc' 		=> __('Height of Image Area', 'aesop-core' )
+					),
+				),
+				'content' 			=> __( 'Quote Here', 'ba-shortcodes' ),
+				'usage' 			=> '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' 				=> __( 'Section quote area with background and color controls.','aesop-core' )
+			),
+			'image_back' 			=> array(
+				'name' 				=> __('Image Back', 'aesop-core'),
+				'type' 				=> 'wrap',
+				'atts' 				=> array(
+					'img' 			=> array(
+						'values' 	=> array( ),
+						'default' 	=> '#',
+						'desc' 		=> __( 'Image URL', 'aesop-core' )
+					),
+					'color' 		=> array(
+						'values' 	=> array(),
+						'type'		=> 'color',
+						'default' 	=> '#FFFFFF',
+						'desc' 		=> __('Color of Text', 'aesop-core' )
+					)
+				),
+				'content' 			=> __( 'Your normal content goes here.', 'ba-shortcodes' ),
+				'usage' 			=> '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' 				=> __( 'Creates a background image section, with text in the middle.','aesop-core' )
+			),
+			'columns' 				=> array(
+				'name' 				=> __('Magazine Columns', 'aesop-core'),
+				'type' 				=> 'wrap',
+				'atts' 				=> array(
+					'num' 			=> array(
+						'values' 	=> array( ),
+						'default' 	=> 2,
+						'desc' 		=> __( 'Number of Columns', 'aesop-core' )
+					)
+				),
+				'content' 			=> __( 'All your normal text goes here.', 'ba-shortcodes' ),
+				'usage' 			=> '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' 				=> __( 'Wraps your text in magazine style columns.','aesop-core' )
+			),
+			'chapter_head' 			=> array(
+				'name' 				=> __('Chapter Heading', 'aesop-core'),
+				'type' 				=> 'single',
+				'atts' 				=> array(
+					'num' 			=> array(
+						'values' 	=> array( ),
+						'default' 	=> 1,
+						'desc' 		=> __( 'What Chapter is This?', 'aesop-core' )
+					)
+				),
+				'content' 			=> __( 'Null here', 'ba-shortcodes' ),
+				'usage' 			=> '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' 				=> __( 'Creates the scroll to point, as a chapter heading.','aesop-core' )
+			),
+			'video' 				=> array(
+				'name' 				=> __('Video Section', 'aesop-core'),
+				'type' 				=> 'single',
+				'atts' 				=> array(
+					'src' 			=> array(
+						'values' 	=> array(
+							__('vimeo', 'aesop-core'),
+							__('youtube', 'aesop-core'),
+							__('kickstarter', 'aesop-core'),
+							__('viddler', 'aesop-core'),
+							__('dailymotion', 'aesop-core')
+						),
+						'default' 	=> 'vimeo',
+						'desc' 		=> __('Video Source', 'aesop-core' )
+					),
+					'id' 			=> array(
+						'values' 	=> array( ),
+						'default' 	=> '',
+						'desc' 		=> __( 'Video ID', 'aesop-core' )
+					)
+				),
+				'content' 			=> __( 'Null here', 'ba-shortcodes' ),
+				'usage'				=> '[aesop-social-icon type="twitter-icon" link="http://link.com" target="_blank"]',
+				'desc' 				=> __( 'Creates a video section.','aesop-core' )
 			)
 		);
 
 		if ( $shortcode )
-			return apply_filters('aesop_avail_shortcodes', $shortcodes[$shortcode]);
+			return apply_filters('aesop_avail_components', $shortcodes[$shortcode]);
 		else
-			return apply_filters('aesop_avail_shortcodes', $shortcodes);
+			return apply_filters('aesop_avail_components', $shortcodes);
 	}
 }
 
