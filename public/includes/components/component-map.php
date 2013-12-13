b6a79d9c690212d7a774b2f91ca83b60dd333444
Bearded Avenger <email@nickhaskins.com>
2013-12-13 11:53:04 -0600

    But fear not as programmers have lots of hair.
    fix boxes, multiple map markers working

--- public/includes/components/component-map.php
+++ public/includes/components/component-map.php
@@ -24,6 +24,7 @@ if (!function_exists('aesop_map_shortcode')) {
 	}
 
 }
+
 class AesopMapComponent {
 
 	public function __construct(){
@@ -35,24 +36,40 @@ class AesopMapComponent {
 
 		global $post;
 
+		$markers = get_post_meta(get_the_ID(),'aesop_map_component_locations', false);
+		$start = get_post_meta($post->ID,'aesop_map_start', true);
+
 		if( has_shortcode( $post->post_content, 'aesop_map') )  { ?>
 			<script>
 
 				var map = L.map('map',{
 					scrollWheelZoom: false,
 					zoom:12,
-					center: [51.505, -0.09]
+					center: [51.5, -0.09]
 				});
-
 
 				L.tileLayer('http://{s}.tile.cloudmade.com/4595fbb0139f4a8b9ccbd1b150016109/997/256/{z}/{x}/{y}.png', {
 					maxZoom: 18,
 					attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 				}).addTo(map);
 
+				L.marker([51.48, -0.09]).addTo(map).bindPopup("Sup").openPopup();
 
-				L.marker([51.5, -0.09]).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+				<?php for($i=1;$i<=3;$i++){
 
+					foreach($markers as $marker):
+
+						$lat = $marker['lat'];
+						$long = $marker['long'];
+						$text = $marker['content'];
+
+						$loc = $lat.','.$long;
+
+						?> L.marker([<?php echo $loc;?>]).addTo(map).bindPopup("<?php echo $text;?>").openPopup(); <?php
+
+					endforeach;
+				}
+				?>
 			</script>
 
 		<?php }
@@ -61,6 +78,11 @@ class AesopMapComponent {
 	function aesop_map_meta( array $meta_boxes ) {
 
 		$opts = array(
+			array(
+				'id'			=> 'aesop_map_start',
+				'name'			=> __('Starting Coordinates', 'aesop-core'),
+				'type'			=> 'text',
+			),
 			array(
 				'id' 			=> 'aesop_map_component_locations',
 				'name' 			=> __('Map Locations', 'aesop-core'),
@@ -71,13 +93,18 @@ class AesopMapComponent {
 				'sortable'		=> true,
 				'fields' 		=> array(
 					array(
-						'id' 	=> 'flacker_extended_meta_prop',
-						'name' 	=> 'Coordinates',
+						'id' 	=> 'lat',
+						'name' 	=> 'Latitude',
 						'type' 	=> 'text'
 					),
 					array(
-						'id' 	=> 'flacker_extended_meta_value',
-						'name' 	=> 'Value',
+						'id' 	=> 'long',
+						'name' 	=> 'Longitude',
+						'type' 	=> 'text'
+					),
+					array(
+						'id' 	=> 'content',
+						'name' 	=> 'Marker Text',
 						'type' 	=> 'text'
 					)
 				)
