6b956e6c5f58460fcb422b8e6d600324bbbd17bf
Bearded Avenger <email@nickhaskins.com>
2013-12-13 10:14:47 -0600

    the hackathon had ended
    kill mouse wheel scrolling

--- public/includes/components/component-map.php
+++ public/includes/components/component-map.php
@@ -34,40 +34,20 @@ if (!function_exists('aesop_map_loader')){
 		if( has_shortcode( $post->post_content, 'aesop_map') )  { ?>
 			<script>
 
-				var map = L.map('map').setView([51.505, -0.09], 13);
+				var map = L.map('map',{
+					scrollWheelZoom: false,
+					zoom:12,
+					center: [51.505, -0.09]
+				});
 
-				L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+
+				L.tileLayer('http://{s}.tile.cloudmade.com/4595fbb0139f4a8b9ccbd1b150016109/997/256/{z}/{x}/{y}.png', {
 					maxZoom: 18,
 					attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 				}).addTo(map);
 
 
-				L.marker([51.5, -0.09]).addTo(map)
-					.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-				L.circle([51.508, -0.11], 500, {
-					color: 'red',
-					fillColor: '#f03',
-					fillOpacity: 0.5
-				}).addTo(map).bindPopup("I am a circle.");
-
-				L.polygon([
-					[51.509, -0.08],
-					[51.503, -0.06],
-					[51.51, -0.047]
-				]).addTo(map).bindPopup("I am a polygon.");
-
-
-				var popup = L.popup();
-
-				function onMapClick(e) {
-					popup
-						.setLatLng(e.latlng)
-						.setContent("You clicked the map at " + e.latlng.toString())
-						.openOn(map);
-				}
-
-				map.on('click', onMapClick);
+				L.marker([51.5, -0.09]).addTo(map).bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 
 			</script>
 
