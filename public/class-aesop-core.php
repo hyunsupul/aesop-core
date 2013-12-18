b93efb9c25db89603c2f631a4362fb8169f91622
Bearded Avenger <email@nickhaskins.com>
2013-12-18 16:30:52 -0600

    minify

--- public/class-aesop-core.php
+++ public/class-aesop-core.php
@@ -277,7 +277,7 @@ class Aesop_Core {
 		wp_enqueue_script('jquery');
 
 		wp_enqueue_style('ai-core-style', AI_CORE_URL.'/public/assets/css/style.css', AI_CORE_VERSION, true);
-		wp_enqueue_script('ai-core', AI_CORE_URL.'/public/assets/js/ai-core.js', array('jquery'), AI_CORE_VERSION, true);
+		wp_enqueue_script('ai-core', AI_CORE_URL.'/public/assets/js/ai-core-min.js', array('jquery'), AI_CORE_VERSION, true);
 	}
 
 	/**
