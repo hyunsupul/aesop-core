da8f9edce23d662ffc1ea9e69e0e27ee1efae642
Bearded Avenger <email@nickhaskins.com>
2013-12-20 12:23:11 -0600

    change component heading to all in one post

--- public/includes/components/component-heading.php
+++ public/includes/components/component-heading.php
@@ -13,15 +13,67 @@ if (!function_exists('aesop_chapter_shortcode')){
 			'img' => ''
 		);
 		$atts = shortcode_atts($defaults, $atts);
+		$hash = rand();
 
 		ob_start();
 		?>
-		<section class="aesop-article-chapter-wrap default-cover">
+		<section id="chapter-hash-<?php echo $hash;?>" class="aesop-article-chapter-wrap default-cover">
 			<div class="aesop-article-chapter clearfix" style="background:url('<?php echo $atts['img'];?>') center center;background-size:cover;">
-				<h1 class="aesop-cover-title" itemprop="title"><?php echo $atts['title'];?></h1>
+				<h1 class="aesop-cover-title aesop-chapter-title" itemprop="title"><?php echo $atts['title'];?></h1>
 			</div>
 		</section>
 		<?php
 		return ob_get_clean();
 	}
 }
+
+if (!function_exists('aesop_chapter_heading_loader')){
+
+	add_action('wp','aesop_chapter_heading_loader');
+	function aesop_chapter_heading_loader() {
+
+		global $post;
+
+		if( has_shortcode( $post->post_content, 'aesop_chapter') )  { 
+
+			new AesopChapterHeadingComponent;
+
+		}
+	}
+
+}
+
+class AesopChapterHeadingComponent {
+
+	function __construct(){
+		add_action('wp_footer', array($this,'aesop_chapter_loader'),21);
+	}
+
+	function aesop_chapter_loader(){
+
+		?>
+			<script>
+			jQuery(document).ready(function(){
+
+				jQuery('.aesop-entry-content').scrollNav({
+				    sections: '.aesop-article-chapter-wrap',
+				    arrowKeys: true,
+				    insertTarget: '.aesop-story-header',
+				    insertLocation: 'appendTo',
+				    showTopLink: false,
+				    showHeadline: false,
+				    scrollOffset: 36,
+				});
+
+			});
+
+			</script>
+
+		<?php 
+	}
+}
+
+
+
+
+
