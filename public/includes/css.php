522290a4709a211c3275be8c98c49435a73c3f96
Bearded Avenger <email@nickhaskins.com>
2014-07-05 16:59:56 -0500

    comments

--- public/includes/css.php
+++ public/includes/css.php
@@ -1,27 +1,45 @@
 <?php
 
+/**
+* Allows theme developers to conditionally load component styles with 
+* add_theme_support('aesop-component-styles', array('component-name'));
+*
+* @since 1.0.9
+*
+*/
+
 class aiCoreCSSMerger {
+
 	function __construct(){
 
 		add_action('wp_enqueue_scripts', array($this,'merger'),11);
 	}
 
+	/**
+	*
+	*	merges styles inline into head
+	*	@todo - this logic could stand to be shortened
+	*
+	*/
 	function merger(){
 
 		$css = '';
 
-		if ( current_theme_supports('aesop-component-styles', 'test') ) {
+		// test support
+		if ( current_theme_supports( 'aesop-component-styles', 'test' ) ) {
 
 			$css = file_get_contents(AI_CORE_DIR.'/public/assets/css/test.css');
 
 		}
 
-		if ( current_theme_supports('aesop-component-styles', 'quote') ) {
+		// test more support
+		if ( current_theme_supports( 'aesop-component-styles', 'quote' ) ) {
 
 			$css .= file_get_contents(AI_CORE_DIR.'/public/assets/css/quote.css');
 		}
 
 		wp_add_inline_style('ai-core-style', $css);
 	}
+
 }
 new aiCoreCSSMerger;
