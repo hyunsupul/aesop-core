5e3f07c1fd9d210c046b75501790d130e8ed0486
Bearded Avenger <email@nickhaskins.com>
2014-08-13 13:48:28 -0500

    style tweaks to new UI

--- admin/includes/generator_blob.php
+++ admin/includes/generator_blob.php
@@ -56,7 +56,6 @@ foreach( $codes as $slug => $shortcode ) {
 		$return .= '<p><label>' . __( 'Content', 'aesop-shortcodes' ) . '</label><input type="text" name="aesop-generator-content" id="aesop-generator-content" value="' . $shortcode['content'] . '" /></p>';
 	}
 
-	$return .= '<p class="aesop-component-description">Description:&nbsp; '.$shortcode['desc'].'</p>';
 	$return .= '<p class="aesop-buttoninsert-wrap"><a href="#" id="aesop-generator-insert">' . __( 'Insert Component', 'aesop-shortcodes' ) . '</a></p> ';
 
 	$return .= '<input type="hidden" name="aesop-generator-result" id="aesop-generator-result" value="" />';
