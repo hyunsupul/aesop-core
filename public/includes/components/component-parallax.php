84a2dedd984e582a2ebea1ec5a13930345efd322
Bearded Avenger <email@nickhaskins.com>
2014-08-30 13:46:39 -0500

    tweak offset position of floater

--- public/includes/components/component-parallax.php
+++ public/includes/components/component-parallax.php
@@ -18,7 +18,7 @@ if (!function_exists('aesop_parallax_shortcode')){
 			'floatermedia' 		=> '',
 			'floaterposition' 	=> 'right',
 			'floaterdirection'	=> 'up',
-			'floateroffset'		=> 100,
+			'floateroffset'		=> 0,
 			'caption'			=> '',
 			'captionposition' 	=> 'bottom-left',
 			'lightbox' 			=> false
@@ -62,14 +62,14 @@ if (!function_exists('aesop_parallax_shortcode')){
 		        			<?php if ( 'on' == $atts['floater'] ) {?>
 								var obj = jQuery('.aesop-parallax-sc.aesop-parallax-sc-<?php echo $unique;?> .aesop-parallax-sc-floater');
 						       	function scrollParallax(){
-						       	    var height 			= <?php echo $height;?>,
-        	        					offset 			= jQuery('.aesop-parallax-sc.aesop-parallax-sc-<?php echo $unique;?>').offset().top,
+						       	    var height 			= jQuery(obj).height(),
+        	        					offset 			= jQuery(obj).offset().top,
 						       	    	scrollTop 		= jQuery(window).scrollTop(),
 						       	    	windowHeight 	= jQuery(window).height(),
 						       	    	floater 		= Math.round( (offset - scrollTop - <?php echo absint(sanitize_text_field($atts['floateroffset']));?>) * 0.1);
 
 						       	    // only run parallax if image in view
-						       	    if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+						       	    if (offset >= scrollTop + windowHeight) {
 										return;
 									}
 
