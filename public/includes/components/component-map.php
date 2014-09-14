57fb3130f79ccc9f2c53b220c5986b211ae2eeae
Bearded Avenger <email@nickhaskins.com>
2014-09-14 08:44:24 -0500

    Maps: automatic marker fallback if view empty
    if the user has not entered a starting coordinate, fallback to the first coordinate that they have entered and warn logged in users
    
    fixes #66

--- public/includes/components/component-map.php
+++ public/includes/components/component-map.php
@@ -39,10 +39,10 @@ class AesopMapComponent {
 
 		global $post;
 
-		$markers = isset( $post ) ? get_post_meta($post->ID,'aesop_map_component_locations', false) : false;
-		$start = isset( $post ) ? get_post_meta($post->ID,'aesop_map_start', true) : false;
-		$mapboxid = get_option('ase_mapbox_id','aesopinteractive.hkoag9o3');
-		$zoom = isset( $post ) && get_post_meta($post->ID,'aesop_map_component_zoom', true) ? get_post_meta($post->ID,'aesop_map_component_zoom', true) : 12;
+		$markers 	= isset( $post ) ? get_post_meta($post->ID,'aesop_map_component_locations', false) : false;
+		$start 		= isset( $post ) && self::get_start_meta($post->ID) ? self::get_start_meta($post->ID) : self::start_fallback($markers);
+		$mapboxid 	= get_option('ase_mapbox_id','aesopinteractive.hkoag9o3');
+		$zoom 		= isset( $post ) && get_post_meta($post->ID,'aesop_map_component_zoom', true) ? get_post_meta($post->ID,'aesop_map_component_zoom', true) : 12;
 
 		$default_location 	= is_single();
 		$location 			= apply_filters( 'aesop_map_component_appears', $default_location );
@@ -55,7 +55,12 @@ class AesopMapComponent {
 
 				if($markers):
 
-					?>
+					if ( !self::get_start_meta($post->ID) && is_user_logged_in() ) { ?>
+
+						jQuery('#aesop-map-component').before('<div class="aesop-error aesop-content"><?php echo __("Looks like you didn\'t specify a starting coordinate, so we\'re using the first one you entered.","aesop-core");?></div>');
+
+					<?php } ?>
+
 					var map = L.map('aesop-map-component',{
 						scrollWheelZoom: false,
 						zoom: <?php echo wp_filter_nohtml_kses( round( $zoom ) );?>,
@@ -79,15 +84,69 @@ class AesopMapComponent {
 
 					endforeach;
 
-				else:
+				else: ?>
 
-					?>jQuery('#aesop-map-component').append('<div class="aesop-error aesop-content"><?php echo __("Your map appears to be empty! Setup and configure your map markers for this post.","aesop-core");?></div>');<?php
+					jQuery('#aesop-map-component').append('<div class="aesop-error aesop-content"><?php echo __("Your map appears to be empty! Setup and configure your map markers for this post.","aesop-core");?></div>');
 
-				endif;
+				<?php endif;
 				?>
 			</script>
 
 		<?php }
+	}
+
+	/**
+	*
+	*	Retrieve the starting coordinate
+	*
+	*	@param $post_id - int
+	* 	@return starting coordinate
+	* 	@since 1.1
+	*/
+	function get_start_meta($post_id = 0){
+
+		// bail if no post id set
+		if ( empty( $post_id ) )
+			return;
+
+  		$meta = get_post_meta( $post_id, 'aesop_map_start', true );
+
+  		return empty( $meta ) ? null : $meta;
+
+	}
+
+	/**
+	*
+	*	If the user has not entered a starting view coordinate,
+	*	then fallback to the first coordinate entered if present.
+	*
+	*	@param $markers - array - gps coordinates entered aspost meta within respective post
+	*	@return first gps marker found
+	* 	@since 1.1
+	*
+	*/
+	function start_fallback( $markers ) {
+
+		// bail if no markers found
+		if( empty( $markers ) )
+			return;
+
+		$i = 0;
+
+		foreach ( $markers as $marker ) { $i++;
+
+			$lat 	= sanitize_text_field($marker['lat']);
+			$long 	= sanitize_text_field($marker['long']);
+
+			$mark 	= sprintf('%s,%s',$lat,$long);
+
+			if ( $i == 1 );
+				break;
+
+		}
+
+		return $mark;
+
 	}
 
 }
