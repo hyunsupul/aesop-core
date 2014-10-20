603d16e1ee5946dec7b4a176a7b7d24b4f4721dd
Bearded Avenger <email@nickhaskins.com>
2014-10-20 16:42:54 -0500

    data sanitize - timeline component

--- public/includes/components/component-timeline.php
+++ public/includes/components/component-timeline.php
@@ -20,9 +20,9 @@ if (!function_exists('aesop_timeline_stop_shortcode')){
 		$actiontop = do_action('aesop_timeline_before'); //action
 		$actionbottom = do_action('aesop_timeline_after'); //action
 
-		$out = sprintf('%s<h2 class="aesop-timeline-stop" %s>%s</h2>',$actiontop, $datatitle, $atts['num']);
+		$out = sprintf('%s<h2 class="aesop-timeline-stop" %s>%s</h2>',$actiontop, $datatitle, $atts['num'] );
 
-		return apply_filters('aesop_timeline_output',$out);
+		return apply_filters('aesop_timeline_output', $out);
 	}
 }
 
@@ -75,14 +75,14 @@ class AesopTimelineComponent {
 
 				jQuery('body').append('<div class="aesop-timeline"></div>');
 
-				jQuery('<?php echo $contentClass;?>').scrollNav({
+				jQuery('<?php echo sanitize_html_class( $contentClass );?>').scrollNav({
 				    sections: '.aesop-timeline-stop',
 				    arrowKeys: true,
 				    insertTarget: '<?php echo $appendTo;?>',
 				    insertLocation: 'appendTo',
 				    showTopLink: false,
 				    showHeadline: false,
-				    scrollOffset: <?php echo $timelineOffset;?>,
+				    scrollOffset: <?php echo (int) $timelineOffset;?>,
 				});
 
 				jQuery('.aesop-timeline-stop').each(function(){
