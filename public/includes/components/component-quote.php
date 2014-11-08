f6c6543eb1be49df2ef4d88ab880844b1db142fa
Bearded Avenger <email@nickhaskins.com>
2014-11-08 08:32:40 -0600

    fix escaping vars quote component

--- public/includes/components/component-quote.php
+++ public/includes/components/component-quote.php
@@ -40,7 +40,7 @@ if (!function_exists('aesop_quote_shortcode')){
 		$bgimg = $atts['img'] ? sprintf('background-image:url(%s);background-size:cover;background-position:center center',esc_url( $atts['img'] )) : false;
 
 		// set styles
-		$style = $atts['background'] || $atts['text'] || $atts['height'] || $atts['width'] ? sprintf('style="background-color:%s;%s;color:%s;height:%s;width:%s;"',esc_html( $atts['background'] ), $bgimg, esc_html( $atts['text'] ), esc_html( $atts['height'] ), esc_html( $atts['width'] )) : false;
+		$style = $atts['background'] || $atts['text'] || $atts['height'] || $atts['width'] ? sprintf('style="background-color:%s;%s;color:%s;height:%s;width:%s;"',esc_attr( $atts['background'] ), $bgimg, esc_attr( $atts['text'] ), esc_attr( $atts['height'] ), esc_attr( $atts['width'] )) : false;
 
 		$isparallax = 'on' == $atts['parallax'] ? 'quote-is-parallax' : false;
 		$lrclass	= 'left' == $atts['direction'] || 'right' == $atts['direction'] ? 'quote-left-right' : false;
@@ -52,20 +52,20 @@ if (!function_exists('aesop_quote_shortcode')){
 		$cite = $atts['cite'] ? apply_filters('aesop_quote_component_cite',sprintf('<cite class="aesop-quote-component-cite">%s</cite>',esc_html( $atts['cite'] ))) : null;
 
 		//align
-		$align = $atts['align'] ? sprintf('aesop-component-align-%s', $atts['align']) : null;
+		$align = $atts['align'] ? sprintf('aesop-component-align-%s', esc_attr($atts['align'])) : null;
 
 		ob_start();
 
 		do_action('aesop_quote_before'); //action
 		?>
-			<div id="aesop-quote-component-<?php echo esc_html( $unique );?>" class="aesop-component aesop-quote-component <?php echo sanitize_html_class( $classes ).' '.sanitize_html_class( $align ).' '.sanitize_html_class( $contentwidth ).' '.sanitize_html_class( $isparallax ).' '.sanitize_html_class( $lrclass ).' ';?>" <?php echo $style;?>>
+			<div id="aesop-quote-component-<?php echo esc_attr( $unique );?>" class="aesop-component aesop-quote-component <?php echo sanitize_html_class( $classes ).' '.sanitize_html_class( $align ).' '.sanitize_html_class( $contentwidth ).' '.sanitize_html_class( $isparallax ).' '.sanitize_html_class( $lrclass ).' ';?>" <?php echo $style;?>>
 
 				<!-- Aesop Core | Quote -->
 				<script>
 					jQuery(document).ready(function(){
 
-						var moving 		= jQuery('#aesop-quote-component-<?php echo esc_html( $unique );?> blockquote'),
-							component   = jQuery('#aesop-quote-component-<?php echo esc_html( $unique );?>');
+						var moving 		= jQuery('#aesop-quote-component-<?php echo esc_attr( $unique );?> blockquote'),
+							component   = jQuery('#aesop-quote-component-<?php echo esc_attr( $unique );?>');
 
 						// if parallax is on and we're not on mobile
 						<?php if ( 'on' == $atts['parallax'] && !wp_is_mobile() ) { ?>
@@ -117,10 +117,10 @@ if (!function_exists('aesop_quote_shortcode')){
 
 				<?php do_action('aesop_quote_inside_top'); //action ?>
 
-				<blockquote class="<?php echo sanitize_html_class( $align );?>" style="font-size:<?php echo esc_html( $size);?>;">
+				<blockquote class="<?php echo sanitize_html_class( $align );?>" style="font-size:<?php echo esc_attr( $size);?>;">
 					<?php echo esc_html( $atts['quote'] );?>
 
-					<?php echo $cite;?>
+					<?php echo esc_html( $cite );?>
 				</blockquote>
 
 				<?php do_action('aesop_quote_inside_bottom'); //action ?>
