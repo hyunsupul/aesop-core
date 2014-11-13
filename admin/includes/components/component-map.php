4f8ea86b9ced5cf08d3dd5eccd2411c108399b01
Bearded Avenger <email@nickhaskins.com>
2014-11-12 20:57:58 -0600

    away with the old map marker metabox yay

--- admin/includes/components/component-map.php
+++ admin/includes/components/component-map.php
@@ -8,10 +8,6 @@ class AesopMapComponentAdmin {
 
 	public function __construct(){
 
-		// old meta
-		// @todo - retire before 1.3 goes out
-		add_filter( 'cmb_meta_boxes', array($this,'aesop_map_meta') );
-
 		// new maps
 		add_action( 'add_meta_boxes', 					array($this,'new_map_box') );
 		add_action( 'admin_enqueue_scripts', 			array($this,'new_map_assets') );
@@ -24,67 +20,6 @@ class AesopMapComponentAdmin {
 
 		add_action('aesop_admin_styles', 		array($this, 'icon') );
 		add_filter('aesop_avail_components',	array($this, 'options'));
-
-	}
-
-	/**
-	*
-	*	Create metabox to store coordinates for maps
-	*	@since 1.0
-	*	@todo map these to the new map meta keys and retire this beofre 1.3 goes out
-	*/
-	function aesop_map_meta( array $meta_boxes ) {
-
-		$opts = array(
-			array(
-				'id'			=> 'aesop_map_start',
-				'name'			=> __('Starting Coordinates', 'aesop-core'),
-				'type'			=> 'text',
-			),
-			array(
-				'id'			=> 'aesop_map_component_zoom',
-				'name'			=> __('Default Zoom Level', 'aesop-core'),
-				'type'			=> 'text',
-				'desc'			=> __('The larger the number, the more zoomed in the default will be. Limit is 20. Default is 12.','aesop-core')
-			),
-			array(
-				'id' 			=> 'aesop_map_component_locations',
-				'name' 			=> __('Map Markers', 'aesop-core'),
-				'type' 			=> 'group',
-				'repeatable'     => true,
-				'repeatable_max' => 20,
-				'desc'			=> __('Assign latitude and longitude for each marker.', 'aesop-core'),
-				'fields' 		=> array(
-					array(
-						'id' 	=> 'lat',
-						'name' 	=> __('Latitude', 'aesop-core'),
-						'type' 	=> 'text',
-						'cols'	=> 6
-					),
-					array(
-						'id' 	=> 'long',
-						'name' 	=> __('Longitude', 'aesop-core'),
-						'type' 	=> 'text',
-						'cols'	=> 6
-					),
-					array(
-						'id' 	=> 'content',
-						'name' 	=> __('Marker Text', 'aesop-core'),
-						'type' 	=> 'textarea',
-						'cols'	=> 12
-					)
-				)
-			),
-		);
-
-		$meta_boxes[] = array(
-			'title' 	=> __('Map Component Locations', 'aesop-core'),
-			'pages' 	=> apply_filters('aesop_map_meta_location','post'),
-			'context' 	=> 'side',
-			'fields' 	=> $opts
-		);
-
-		return $meta_boxes;
 
 	}
 
