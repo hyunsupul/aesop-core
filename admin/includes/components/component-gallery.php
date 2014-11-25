9f6e90a30db9e6c41eee3988d0eb5b6d173a508f
Bearded Avenger <email@nickhaskins.com>
2014-11-24 20:44:40 -0600

    fix dead assignments

--- admin/includes/components/component-gallery.php
+++ admin/includes/components/component-gallery.php
@@ -331,17 +331,16 @@ class AesopGalleryComponentAdmin {
 		// get the posts with the maps shortode
 		$posts = get_posts( array( 'post_type' => array('ai_galleries'), 'posts_per_page' => -1 ) );
 
-		$count = 0;
-
 		if ( $posts ) :
 			foreach( $posts as $post ) {
+
 				$id = $post->ID;
 
-				$old_image_ids 		= get_post_gallery( $post->ID, false);
+				$old_image_ids 		= get_post_gallery( $id, false);
 				$old_image_ids 		= $old_image_ids['ids'];
 
 				if ( ! empty ( $old_image_ids ) ) {
-					add_post_meta( $post->ID, '_ase_gallery_images', $old_image_ids );
+					add_post_meta( $id, '_ase_gallery_images', $old_image_ids );
 				}
 			}
 		endif;
