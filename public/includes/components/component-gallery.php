730942f4d6bddeda18f70cc430579357207e12e5
Bearded Avenger <email@nickhaskins.com>
2015-01-31 09:13:26 -0600

    fix class exists for new editor

--- public/includes/components/component-gallery.php
+++ public/includes/components/component-gallery.php
@@ -82,7 +82,7 @@ class AesopCoreGallery {
 					}
 
 					// provide the edit link to the backend edit if Aesop Editor is not active
-					if ( !class_exists('Aesop_Editor') && is_user_logged_in() && current_user_can('edit_posts') ) {
+					if ( !class_exists('Lasso') && is_user_logged_in() && current_user_can('edit_posts') ) {
 						$url = admin_url( 'post.php?post='.$gallery_id.'&action=edit' );
 						$edit_gallery = __('edit gallery', 'aesop-core');
 						printf('<a class="aesop-gallery-edit aesop-content" href="%s" target="_blank" title="%s">(%s)</a>',$url, $edit_gallery, $edit_gallery );
@@ -92,7 +92,7 @@ class AesopCoreGallery {
 
 				if ( empty( $gallery_id ) && is_user_logged_in() && current_user_can('edit_posts') ) {
 
-					if ( class_exists('Aesop_Editor') ) {
+					if ( class_exists('Lasso') ) {
 
 						?><div class="aesop-editor--empty-gallery"><?php
 							_e('Setup a gallery by clicking the <span class="aesop-icon-gear"></span> icon above.', 'aesop-core');
