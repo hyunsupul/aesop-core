0d1d19c539d16ca1bc937ba490b8682484eb8468
hyunster <hyunsupul@yahoo.com>
2017-07-01 11:30:56 -0400

    updated welcome page

--- admin/includes/class.welcome.php
+++ admin/includes/class.welcome.php
@@ -61,10 +61,12 @@ class aesopCoreWelcome {
 		  		<div class="aesop--welcome__section aesop--welcome__section--top">
 			  		<img src="<?php echo AI_CORE_URL.'/public/assets/img/aesop-logo.svg';?>">
 			    	<h1><?php _e( 'Welcome to Aesop Story Engine', 'aesop-core' );?></h1>
+					<h3><a href="http://aesopstoryengine.com/about/" target="_blank"><?php _e( 'Introduction', 'aesop-core' );?></a></h3>
 			    	<h3><?php _e( 'Get started in a snap with our quick start guide', 'aesop-core' );?></h3>
 			    	<ul class="aesop--welcome__social">
 			    		<li><a href="http://twitter.com/aesopinteractiv" target="_blank"><i class="dashicons dashicons-twitter"></i> <?php _e( 'Twitter', 'aesop-core' );?></a></li>
 			    		<li><a href="http://facebook.com/aesopinteractive" target="_blank"><i class="dashicons dashicons-facebook"></i> <?php _e( 'Facebook', 'aesop-core' );?></a></li>
+						<li><a href="http://aesopstoryengine.com/be-social/" target="_blank"><i class="dashicons dashicons-email"></i> <?php _e( 'Sign up for news and exclusive offers!', 'aesop-core' );?></a></li>
 			    	</ul>
 			    </div>
 
@@ -72,6 +74,11 @@ class aesopCoreWelcome {
 			   		<h2><?php _e( 'Quick Start', 'aesop-core' );?></h2>
 			   		<p class="aesop--welcome__lead"><?php _e( 'It\'s easy to get Aesop Story Engine implemented into your theme. Just follow these short steps below and you\'ll be on your way!', 'aesop-core' );?></p>
 					<ul class="aesop--welcome__steps">
+					
+					    <li>
+							<strong><?php _e( 'Introduction', 'aesop-core' );?></strong>
+							<p>Check <a href="http://http://aesopstoryengine.com/about/"><?php _e( 'Intro/Demo page ', 'aesop-core' );?></a> <?php _e(' to see what Aesop has to offer.', 'aesop-core' )?></p>
+						</li>
 						<li>
 							<strong><?php _e( 'Development Information', 'aesop-core' );?></strong>
 							<p>Check <a href="http://aesopstoryengine.com/developers/"><?php _e( 'Developer page ', 'aesop-core' );?></a> <?php _e(' for more information about theme development.', 'aesop-core' )?></p>
@@ -90,7 +97,7 @@ class aesopCoreWelcome {
 				</div>
 
 			   	<div class="aesop--welcome__section aesop--welcome__section--addons">
-			   		<h2><?php _e( 'Simpler, Faster, Easier Writing in WordPress', 'aesop-core' );?></h2>
+			   		<h2>Editus: <?php _e( 'Simpler, Faster, Easier Writing in WordPress', 'aesop-core' );?></h2>
 			   		<p><?php _e( 'Stop wasting time constantly previewing your post, and edit and create from the front-end of your site with Editus, our front-end editor that goes perfectly with Aesop Story Engine. Visit the site and enter your email for a 25% discount!', 'aesop-core' );?> <a href="https://edituswp.com/?utm_source=aesop-plugin&utm_medium=banner&utm_campaign=aesop-plugin" target="_blank"><?php _e( 'Read More...', 'aesop-core' );?></a></p>
 				</div>
 
