d5fa40ee3372f64323b56cf4b15e433229789ecd
hyunsupul@yahoo.com <hyunsupul@yahoo.com>
2019-02-13 10:14:28 -0500

    2.0.7.1

--- aesop-core.php
+++ aesop-core.php
@@ -13,7 +13,7 @@
  *  Plugin Name:       Aesop Story Engine
  *  Plugin URI:        http://aesopstoryengine.com
  *  Description:       Open-sourced suite of components that empower interactive storytelling.
- *  Version:           2.0.7
+ *  Version:           2.0.7.1
  *  Author:            Aesopinteractive 
  *  Author URI:        http://aesopstoryengine.com
  *  Text Domain:       aesop-core
